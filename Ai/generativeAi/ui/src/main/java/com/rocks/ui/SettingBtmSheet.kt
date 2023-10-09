a7313cc8cd9b99e45589904543a1f0e6b00ce054
aruns <arunsaini065@gmail.com>
2023-10-09 11:09:15 +0530

    ai first commit video

--- Ai/generativeAi/ui/src/main/java/com/rocks/ui/SettingBtmSheet.kt
+++ Ai/generativeAi/ui/src/main/java/com/rocks/ui/SettingBtmSheet.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.rocks.AspectRatio
 import com.rocks.OnBodyHandlerListener
 import com.rocks.ui.databinding.ChooseSettingBtmsheetBinding
+import com.rocks.ui.ratio.CropRatioRecyclerView
 
 class SettingBtmSheet: BottomSheetDialogFragment() {
 
@@ -17,9 +21,86 @@ class SettingBtmSheet: BottomSheetDialogFragment() {
     private val _settingBinding by lazy { ChooseSettingBtmsheetBinding.inflate(layoutInflater) }
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL, R.style.BootomSheetDialogTheme);
+        setStyle(STYLE_NORMAL, R.style.BootomSheetDialogTheme)
+
+        _settingBinding.negativePrompt.addTextChangedListener {
+
+            if (::onBodyHandlerListener.isInitialized){
+
+                onBodyHandlerListener.getHandlerBody().negativePrompt = it.toString()
+
+            }
+
+        }
+
+     _settingBinding.seedTxt.addTextChangedListener {
+
+            if (::onBodyHandlerListener.isInitialized){
+
+                onBodyHandlerListener.getHandlerBody().seed = it.toString()
+
+            }
+
+        }
+
+        _settingBinding.stepsSeekbar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                if (::onBodyHandlerListener.isInitialized){
+
+                    onBodyHandlerListener.getHandlerBody().numbInferenceSteps = seekBar?.progress.toString()
+
+                }
+
+            }
+        })
+
+        _settingBinding.cfgSeekbar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                if (::onBodyHandlerListener.isInitialized){
+
+                    onBodyHandlerListener.getHandlerBody().guidanceScale = seekBar?.progress.toString()
+
+                }
+
+            }
+        })
+
+        _settingBinding.aspectRatioRvStng.iChangeRatioListener = object :CropRatioRecyclerView.IChangeRatioListener{
+
+            override fun onChangeRatio(width: Int, height: Int) {
+
+                if (::onBodyHandlerListener.isInitialized){
+
+                    onBodyHandlerListener.getHandlerBody().aspectRatio = AspectRatio(widthR = width, heightR = height)
+
+                }
+
+            }
+
+
+        }
+
     }
 
     override fun onAttach(context: Context) {
